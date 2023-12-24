8f09d2ba9032decbd5ab3931a122a5de5d4d8fc3
JuliaMichaela <juliaperez@yandex.ru>
2023-12-24 23:39:19 +0300

    changed js

--- script.js
+++ script.js
@@ -4,3 +4,6 @@ console.log (priceJacket)
 const priceSneakers = document.querySelector('#priceSneakers');
 const priceJeans = document.querySelector('#priceJeans');
 const priceBent = document.querySelector('#priceBent');
+console.log (priceSneakers)
+console.log (priceJeans )
+console.log (priceBent)
