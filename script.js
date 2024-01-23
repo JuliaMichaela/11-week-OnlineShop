8fcd9cea61457de5ad86b2b2cd15b0f719c5f689
JuliaMichaela <juliaperez@yandex.ru>
2024-01-23 23:48:34 +0300

    edited

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ console.log (priceJacket.textContent);
 
 const priceSneakers = document.querySelector('#priceSneakers');
 const priceJeans = document.querySelector('#priceJeans');
-const priceBent = document.querySelector('#priceBent');
-console.log (priceSneakers.textContent);
-console.log (priceJeans.textContent);
-console.log (priceBent.textContent);
+// const priceBent = document.querySelector('#priceBent');
+// console.log (priceSneakers.textContent);
+// console.log (priceJeans.textContent);
+// console.log (priceBent.textContent);
 
 
 const result = +priceJeans + +priceSneakers + +priceJeans + +priceBent;
