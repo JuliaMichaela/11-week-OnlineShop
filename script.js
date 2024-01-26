8106fecc1350976aa10a48f1760e6ac7ffbd5b6f
JuliaMichaela <juliaperez@yandex.ru>
2024-01-26 23:47:53 +0300

    create new button

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const priceJacket = document.querySelector('#priceJacket')
 console.log (priceJacket.textContent);
 
-// const priceSneakers = document.querySelector('#priceSneakers');
-// const priceJeans = document.querySelector('#priceJeans');
-// const priceBent = document.querySelector('#priceBent');
-// console.log (priceSneakers.textContent);
-// console.log (priceJeans.textContent);
-// console.log (priceBent.textContent);
+const priceSneakers = document.querySelector('#priceSneakers');
+const priceJeans = document.querySelector('#priceJeans');
+const priceBent = document.querySelector('#priceBent');
+console.log (priceSneakers.textContent);
+console.log (priceJeans.textContent);
+console.log (priceBent.textContent);
 
 
 const result = +priceJeans + +priceSneakers + +priceJeans + +priceBent;
