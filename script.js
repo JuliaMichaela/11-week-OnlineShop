89f1e9ead2d424157d9dea4ca7b06deaae5cc64e
JuliaMichaela <juliaperez@yandex.ru>
2024-01-31 23:50:25 +0300

    3

--- script.js
+++ script.js
@@ -13,4 +13,6 @@ const result = +priceJeans + +priceSneakers + +priceJeans + +priceBent;
 
 console.log (result);
 
-
+console.log (priceSneakers.textContent);
+console.log (priceJeans.textContent);
+console.log (priceBent.textContent);
