e57d0d2410fa088243417f06aafbbfc60a2d6aa0
JuliaMichaela <juliaperez@yandex.ru>
2024-02-16 23:53:07 +0300

    fixed

--- script.js
+++ script.js
@@ -12,5 +12,4 @@ console.log (priceBent.textContent);
 const result = Number(priceJacket.textContent)+Number(priceSneakers.textContent)+ Number(priceJeans.textContent) + Number(priceBent.textContent);
 
 console.log (result);
-console.log (result);
 
